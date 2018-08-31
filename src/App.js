8efdb64988268992674ad6de229f8b7ff65b4735
Tammer Galal <tammergalal20@gmail.com>
2018-08-31 18:31:03 -0400

    working on portolfio and ideas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import Navbar from "./Navbar";
-import Header from "./Header";
+import FaceCheck from "./FaceCheck/components/FaceCheck";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <Header />
+        <Router>
+          <FaceCheck path="/face" />
+        </Router>
       </div>
     );
   }
---
+++ src/FaceCheck/components/FaceCheck.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class FaceCheck extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: ""
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <div className="Search" />
+      </div>
+    );
+  }
+}
+
+export default FaceCheck;
